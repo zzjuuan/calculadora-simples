d4fa48f4a49ea093fec906d3b8f3c8e1063eacf6
Juan Cunha <juangigliotticunha09062006@gmail.com>
2023-09-06 20:30:33 -0300

    Atualizado

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 function addVisor(value) {
-    document.getElementById('visor').value += value;
+    const visor = document.getElementById('visor');
+    const valorAtual = visor.value;
+
+    const ultimoCaracter = valorAtual.charAt(valorAtual.length - 1);
+    const operators = "+-*/÷^x.";
+
+    if (operators.includes(ultimoCaracter) && operators.includes(value)) {
+        document.getElementById('visor').value = valorAtual.substring(0, valorAtual.length - 1) + value;;
+    } else {
+        document.getElementById('visor').value += value;
+    }
 }
 
 function calcular() {
-    let valor = document.getElementById('visor').value;
+    let valor = document.getElementById('visor');
+    const valorAtual = visor.value;
+    const ultimoCaracter = valorAtual.charAt(valorAtual.length - 1);
+    const operators = "+-*/÷^x.";
+
+    if (operators.includes(ultimoCaracter)) {
+        valor = valorAtual.substring(0, valorAtual.length - 1);;
+    }
+
     valor = valor.replace(/÷/g, '/');
     valor = valor.replace(/x/g, '*');
     valor = valor.replace(/\^/g, '**');
+
     const resultado = eval(valor)
-    if(resultado == undefined || resultado == null){
+    if (resultado == undefined || resultado == null) {
         return 0;
-    }else {
-        document.getElementById('visor').value  = resultado;  
+    } else {
+        document.getElementById('visor').value = resultado;
     }
 }
 
